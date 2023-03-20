a622dfd60c976ec1efa15a7a9cb86778ca33112c
kevinlys00 <128156104+kevinlys00@users.noreply.github.com>
2023-03-21 00:12:08 +0900

    206b

--- week2-divide_and_conquer/QUADTREE-kevinlys00_SHORT.cpp
+++ week2-divide_and_conquer/QUADTREE-kevinlys00_SHORT.cpp
@@ -1,2 +1,2 @@
 #include<bits/stdc++.h>
-int i;char S[1010];std::string Z(){std::string a,b,c;a+=S[i];if(S[i++]>'w')b=Z(),c=Z(),a+=Z(),a+=Z();return a+b+c;}int main(){for(scanf("%*d");~scanf("%s",S);std::cout<<Z()<<'\n')i=0;}
+int i;char S[1010];std::string Z(){std::string a,b;a+=S[i];if(S[i++]>'w')b=Z(),b+=Z(),a+=Z(),a+=Z()+b;return a;}int main(){for(scanf("%*d");~scanf("%s",S);std::cout<<Z()<<'\n')i=0;}
