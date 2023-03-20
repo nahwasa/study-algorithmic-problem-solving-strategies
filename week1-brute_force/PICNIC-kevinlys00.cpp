dbc5d780189629b25bce6e06dd9f9b7f0f2e439f
kevinlys00 <128156104+kevinlys00@users.noreply.github.com>
2023-03-20 18:53:53 +0900

    Update PICNIC-kevinlys00.cpp

--- week1-brute_force/PICNIC-kevinlys00.cpp
+++ week1-brute_force/PICNIC-kevinlys00.cpp
@@ -1,19 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int used, Wkr[10][10];
+int used, Wkr[10][10], n, m;
 
-int solve(int idx, int n){
+int solve(){
     if(used==(1<<n)-1) return 1;
     
     int i,ans=0;
-    for(i=idx ; i<n ; i++) if(!(used&(1<<i))) break;
+    for(i=0 ; i<n ; i++) if(!(used&(1<<i))) break;
     if(i==n) return 0;
     used^=1<<i;
     for(int j=i+1 ; j<n ; j++){
         if(Wkr[i][j] && !(used&(1<<j))){
             used^=1<<j;
-            ans+=solve(i,n);
+            ans+=solve();
             used^=1<<j;
         }
     }
@@ -23,19 +23,17 @@ int solve(int idx, int n){
 }
 
 int main(){
-    cin.tie(0);
     int C;
     for(cin >> C ; C-- ; ){
         fill(Wkr[0],Wkr[10],0);
         used=0;
         
-        int n,m; cin>>n>>m;
+        cin>>n>>m;
         while(m--){
             int a,b; cin>>a>>b;
-            if(a<b) Wkr[a][b]=1;
-            else Wkr[b][a]=1;
+            Wkr[a][b]=Wkr[b][a]=1;
         }
         
-        cout<<solve(0,n)<<'\n';
+        cout<<solve()<<'\n';
     }
 }
