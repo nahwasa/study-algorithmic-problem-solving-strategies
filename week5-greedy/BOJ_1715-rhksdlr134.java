293e3000bdbc91f8d80e42b5b3eb900d1708a957
kwanik-kor <rhksdlr134@naver.com>
2023-04-15 16:10:16 +0900

    feat: Week 05 카드 정렬하기 해결

---
+++ week5-greedy/BOJ_1715-rhksdlr134.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.PriorityQueue;
+
+/**
+ * 1. 문제 링크: https://www.acmicpc.net/problem/1715
+ */
+public class greedy_01715_sortCard {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < N; i++) {
+            pq.add(Integer.parseInt(br.readLine()));
+        }
+
+        int tot = 0;
+        while (pq.size() != 1) {
+            int joined = pq.poll() + pq.poll();
+            tot += (joined);
+            pq.add(joined);
+        }
+
+        bw.write(tot + "");
+        br.close();
+        bw.close();
+    }
+}
